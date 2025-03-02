aa4a2ceb3f68581649859bf73bc1cd3389b7ef0e
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-03-02 23:38:00 +0200

    first commit

---
+++ src/test/java/com/asal/insurance_system/InsuranceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asal.insurance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
