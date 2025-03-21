9163d789a2bdfb6e6405950827fc422616d3b565
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-03-21 04:17:24 +0200

    Begin with User Repository Testing

---
+++ src/test/java/com/asal/insurance_system/Repository/UserRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.asal.insurance_system.Repository;
+
+import com.asal.insurance_system.Model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void findByEmailNotFoundTest(){
+        Optional<User> user = userRepository.findByEmail("mm@mail.com");
+        assertFalse(user.isPresent());
+    }
+
+    @Test
+    void findByEmailFoundTest(){
+        Optional<User> user = userRepository.findByEmail("admin@gmail.com");
+        assertTrue(user.isPresent());
+        assertEquals("admin@gmail.com",user.get().getEmail());
+    }
+
+}
