ed0bb03575a89c7c21ec59765a17bd5101635cd6
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-03-21 04:27:29 +0200

    Create User Repo. Test

--- src/test/java/com/asal/insurance_system/Repository/UserRepositoryTest.java
+++ src/test/java/com/asal/insurance_system/Repository/UserRepositoryTest.java
@@ -1,13 +1,9 @@
 package com.asal.insurance_system.Repository;
-
 import com.asal.insurance_system.Model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
