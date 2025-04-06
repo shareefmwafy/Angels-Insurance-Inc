056caac68b0ef16e7b92c10e9cd89dfe28182255
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-04-06 13:53:39 +0300

    Finished Policy CRUD

--- src/main/java/com/asal/insurance_system/Controller/PolicyController.java
+++ src/main/java/com/asal/insurance_system/Controller/PolicyController.java
@@ -1,6 +1,7 @@
 package com.asal.insurance_system.Controller;
 
 import com.asal.insurance_system.DTO.Request.PolicyRequestDTO;
+import com.asal.insurance_system.DTO.Response.PolicyResponseDTO;
 import com.asal.insurance_system.Exception.ResourceNotFoundException;
 import com.asal.insurance_system.Model.Policy;
 import com.asal.insurance_system.Service.PolicyService;
@@ -11,6 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.asal.insurance_system.Exception.ErrorResponseUtil;
+
+import java.util.List;
+
+import static com.asal.insurance_system.Exception.ErrorResponseUtil.createErrorResponse;
 
 @RestController
 @RequestMapping("/api/v1/policy")
@@ -30,12 +35,72 @@ public class PolicyController {
             return new ResponseEntity<>(newPolicy, HttpStatus.CREATED);
         } catch (ResourceNotFoundException ex) {
 
-            return new ResponseEntity<>(ErrorResponseUtil.createErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(createErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
         } catch (Exception ex) {
 
-            return new ResponseEntity<>(ErrorResponseUtil.createErrorResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(createErrorResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    @GetMapping("")
+    public ResponseEntity<?> getAllPolices(){
+        try{
+            List<PolicyResponseDTO> policies = policyService.getAllPolices();
+            if (policies.isEmpty()){
+                return new ResponseEntity<>(createErrorResponse("No Policies Found", HttpStatus.NOT_FOUND),HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(policies, HttpStatus.OK);
+        }
+        catch (Exception ex){
+            return new ResponseEntity<>(createErrorResponse("Error while fetching policies: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    @GetMapping("{id}")
+    public ResponseEntity<?> getPolicyById(@PathVariable Integer id) {
+        try {
+            PolicyResponseDTO policy = policyService.getPolicyById(id);
+            return new ResponseEntity<>(policy, HttpStatus.OK);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(createErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(createErrorResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    @PutMapping("{id}")
+    public ResponseEntity<?> updatePolicy(@PathVariable Integer id, @RequestBody PolicyRequestDTO requestDTO) {
+        try {
+            Policy policy = policyService.updatePolicy(id, requestDTO);
+            return new ResponseEntity<>(policy, HttpStatus.NO_CONTENT);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(createErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(createErrorResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deletePolicy(@PathVariable Integer id) {
+        try {
+            boolean policy = policyService.deletePolicy(id);
+            return new ResponseEntity<>(policy, HttpStatus.NO_CONTENT);
+        } catch (ResourceNotFoundException ex) {
+            return new ResponseEntity<>(createErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(createErrorResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+
+
+
 
 
 
