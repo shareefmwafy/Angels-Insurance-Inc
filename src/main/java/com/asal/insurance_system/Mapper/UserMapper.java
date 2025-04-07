e19f3f2fcd3ad46e8f39d1698a88c4e82ad5ab62
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-04-07 20:11:22 +0300

    Remove print Statment and some comments

--- src/main/java/com/asal/insurance_system/Mapper/UserMapper.java
+++ src/main/java/com/asal/insurance_system/Mapper/UserMapper.java
@@ -13,10 +13,6 @@ public interface UserMapper {
     // map from DTO to Entity
     User mapToEntity (UserDTO dto);
 
-//    @Mapping(target = "authorities", ignore = true)
-//    @Mapping(target = "id", ignore = true)
-//    User mapToUpdatedUser (User user);
-
     @Mapping(target = "id", ignore = true) // Prevents changing the ID
     @Mapping(target = "authorities", ignore = true) // Prevents modifying authorities/roles
     void mapToUpdatedUser(User source, @MappingTarget User target);
