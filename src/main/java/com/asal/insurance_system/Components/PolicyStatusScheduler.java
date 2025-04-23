ad98c8a1eecb0f1b23d01a058196d813e24be0a6
Shareef Mwafy <sh.mwafy1@gmail.com>
2025-04-23 12:05:37 +0300

    Resolve most of Reviewd changes

--- src/main/java/com/asal/insurance_system/Components/PolicyStatusScheduler.java
+++ src/main/java/com/asal/insurance_system/Components/PolicyStatusScheduler.java
@@ -3,6 +3,7 @@ package com.asal.insurance_system.Components;
 import com.asal.insurance_system.Enum.EnumPolicyStatus;
 import com.asal.insurance_system.Model.Policy;
 import com.asal.insurance_system.Repository.PolicyRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,11 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.List;
 
+@Slf4j
 @Component
 public class PolicyStatusScheduler {
 
     private final PolicyRepository policyRepository;
-
-    private Logger logger = LoggerFactory.getLogger(PolicyStatusScheduler.class);
-    @Autowired
     public PolicyStatusScheduler(PolicyRepository policyRepository){
         this.policyRepository = policyRepository;
     }
@@ -33,13 +32,11 @@ public class PolicyStatusScheduler {
             if(policy.getEndDate().isBefore(today) && policy.getPolicyStatus() == EnumPolicyStatus.ACTIVE){
                 policy.setPolicyStatus(EnumPolicyStatus.EXPIRED);
                 policyRepository.save(policy);
-                logger.info("Policy ID " + policy.getId() + " has been marked as EXPIRED");
+                log.info("Policy ID " + policy.getId() + " has been marked as EXPIRED");
             }
-            System.out.println("Policy ID: " + policy.getId());
-            System.out.println("EndDate: " + policy.getEndDate());
-            System.out.println("Today: " + today);
-            System.out.println("Status: " + policy.getPolicyStatus());
-
+            if(log.isTraceEnabled()){
+                log.trace("Policy ID: {}", policy.getId());
+            }
         }
     }
 
