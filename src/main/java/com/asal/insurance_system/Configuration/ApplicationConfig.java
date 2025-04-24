5dcdcb93a90a13d371d1df9900360be2c4a5cd86
Shareef Mwafy <sh.mwafy1@gmail.com>
2025-04-24 09:49:59 +0300

    Use constant for prefixes

--- src/main/java/com/asal/insurance_system/Configuration/ApplicationConfig.java
+++ src/main/java/com/asal/insurance_system/Configuration/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.asal.insurance_system.Configuration;
 import com.asal.insurance_system.Repository.CustomerRepository;
 import com.asal.insurance_system.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
@@ -24,18 +26,20 @@ public class ApplicationConfig {
     private final UserRepository userRepository;
     private final CustomerRepository customerRepository;
 
-    private final Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
+
+    private static final String CUSTOMER_PREFIX = "customer_";
+    private static final String USER_PREFIX = "systemUser_";
     @Bean
     public UserDetailsService userDetailsService(){
         return username -> {
-            if (username.startsWith("customer_")) {
-                String subUsername = username.substring("customer_".length());
-                logger.info("Log in with customer {}", subUsername);
+            if (username.startsWith(CUSTOMER_PREFIX)) {
+                String subUsername = username.substring(CUSTOMER_PREFIX.length());
+                log.info("Log in with customer {}", subUsername);
                 return (UserDetails) customerRepository.findByEmail(subUsername);
             }
-            else if (username.startsWith("systemUser_")) {
-                String subUsername = username.substring("systemUser_".length());
-                logger.info("Log in with system user {}", subUsername);
+            else if (username.startsWith(USER_PREFIX)) {
+                String subUsername = username.substring(USER_PREFIX.length());
+                log.info("Log in with system user {}", subUsername);
                 return userRepository.findByEmail(subUsername)
                         .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
             }
