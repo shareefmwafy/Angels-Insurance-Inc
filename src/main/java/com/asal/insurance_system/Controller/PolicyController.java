e9b773a4b899d9a2e9d962a3fbd7a2c3f420c249
Shareef Mwafy <sh.mwafy1@gmail.com>
2025-05-22 13:48:18 +0300

    Create method get policy by Customer id

--- src/main/java/com/asal/insurance_system/Controller/PolicyController.java
+++ src/main/java/com/asal/insurance_system/Controller/PolicyController.java
@@ -47,12 +47,21 @@ public class PolicyController {
         return new ResponseEntity<>(policies, HttpStatus.OK);
     }
 
-    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER') or hasRole('CUSTOMER')")
     @GetMapping("{id}")
     public ResponseEntity<?> getPolicyById(@PathVariable Integer id) {
         PolicyResponseDTO policy = policyService.getPolicyById(id);
         return new ResponseEntity<>(policy, HttpStatus.OK);
     }
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER') or hasRole('CUSTOMER')")
+    @GetMapping("/byCustomer/{CustomerId}")
+    public ResponseEntity<?> getPolicyByCustomerId(@PathVariable Integer CustomerId) {
+        List<PolicyResponseDTO> policies = policyService.getPoliciesByCustomerId(CustomerId);
+        return new ResponseEntity<>(policies , HttpStatus.OK);
+    }
+
+
 
     @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     @PutMapping("{id}")
